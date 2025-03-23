7a63463296c04c29a41d2364ca9a7ac066cc3dba
AllAryanCoding29 <allaryancoding@gmail.com>
2025-03-23 11:54:39 -0400

    I added a Guessing Game under the Games Folder! Take a look and watch out for upcoming programs!

---
+++ Games/guessTheNumber.py
@@ -0,0 +1,23 @@
+from random import randrange
+print("Hello, welcome to My Guessing Game!")
+print("Enter different numbers from 1 - 100 until you get it right!")
+print("Every time you guess, it will tell you if you are too low, or too high!")
+print("Have Fun!")
+num = randrange(1,101)
+while True:
+    guess = int(input("Enter your guess:"))
+    if guess < num:
+        print("Too low, Try again!")
+    elif guess > num:
+        print("Too high, Try again!")
+    elif guess == num:
+        print("Great job, you guessed it!")
+        restart = input("Type 1 to go again, and Any other key to leave:")
+        if restart == "1":
+            print("Restarting...")
+            num = randrange(1, 101)
+        else:
+            print("Thanks for playing My Guessing Game!")
+            break
+
+
