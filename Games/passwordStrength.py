cc5d38c83ca49bbc4747bdbd970354ec02ccae5a
AllAryanCoding29 <allaryancoding@gmail.com>
2025-03-28 22:33:42 -0400

    I just entered a password strength checker under the Games folder. This project helped me learn a lot about strings as well. Specifically, I learned about new functions like any(), and also learned different ways to iterate through strings. Stay updated for more games and code!

---
+++ Games/passwordStrength.py
@@ -0,0 +1,19 @@
+# Should have 8 characters
+# Should have lowercase and uppercase letters
+# Should include a number
+print("Hello, welcome to My Password Strength Checker. Enter your own password below to test it out, or just to have fun!")
+def check():
+    while True:
+        password = input("Enter your password: ")
+        if len(password) < 8:
+            print("Not strong enough, try again. Tip: Make your password at least 8 characters.")
+        elif not any(char.isdigit() for char in password):
+            print("You have 8 characters, but you are missing a number. Try again.")
+        elif not any(char.isupper() for char in password):
+            print("Add an Uppercase letter. Try again.")
+        elif not any(char.islower() for char in password):
+            print("Add a lowercase letter. Try again.")
+        else:
+            print(f"{password} is a great password. It has 8 characters, Upper and lowercase letters, and a number! Thanks for using My Password Strength Checker!")
+            break
+check()
