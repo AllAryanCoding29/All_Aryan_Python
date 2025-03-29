6f532f923c5a93353a236f9b46f3fe108a43c621
AllAryanCoding29 <allaryancoding@gmail.com>
2025-03-29 09:27:27 -0400

    Small edits to make the code simpler. Look out for upcoming updates!

--- Games/guessTheNumber.py
+++ Games/guessTheNumber.py
@@ -3,7 +3,9 @@ print("Hello, welcome to My Guessing Game!")
 print("Enter different numbers from 1 - 100 until you get it right!")
 print("Every time you guess, it will tell you if you are too low, or too high!")
 print("Have Fun!")
-num = randrange(1,101)
+def generate():
+    return randrange(1,10)
+num = generate()
 while True:
     guess = int(input("Enter your guess:"))
     if guess < num:
@@ -15,7 +17,7 @@ while True:
         restart = input("Type 1 to go again, and Any other key to leave:")
         if restart == "1":
             print("Restarting...")
-            num = randrange(1, 101)
+            num = generate()
         else:
             print("Thanks for playing My Guessing Game!")
             break
--- Games/passwordStrength.py
+++ Games/passwordStrength.py
@@ -2,12 +2,18 @@
 # Should have lowercase and uppercase letters
 # Should include a number
 print("Hello, welcome to My Password Strength Checker. Enter your own password below to test it out, or just to have fun!")
+def check_password_contain_digit(password):
+    for char in password:
+        if char.isdigit():
+            return True
+    return False
+
 def check():
     while True:
         password = input("Enter your password: ")
         if len(password) < 8:
             print("Not strong enough, try again. Tip: Make your password at least 8 characters.")
-        elif not any(char.isdigit() for char in password):
+        elif not check_password_contain_digit(password):
             print("You have 8 characters, but you are missing a number. Try again.")
         elif not any(char.isupper() for char in password):
             print("Add an Uppercase letter. Try again.")
