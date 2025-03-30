7b4e9b22db342de37d65afb930c20e3ce0a365e5
AllAryanCoding29 <allaryancoding@gmail.com>
2025-03-30 09:19:02 -0400

    Small code fixes on the ATM program!

--- Games/atmPinValidation.py
+++ Games/atmPinValidation.py
@@ -2,10 +2,10 @@
 print("Hello, and welcome to My ATM Pin Validator. This does not collect your data or tells you what your ATM pin should be.")
 def check():
     while True:
-        atm = int(input("What ATM pin do you want to check? "))
-        if len(f"{atm}") < 4:
+        atm = input("What ATM pin do you want to check? ")
+        if len(atm) < 4:
             print("An ATM pin has to have at least 4 digits!")
-        elif len(f"{atm}") < 6:
+        elif len(atm) < 6:
             print("Better, but you should have 6 digits to be safe.")
             restart = input("Type c to continue, and press enter to try again.")
             if restart == "c":
@@ -18,6 +18,8 @@ def check():
                     break
             else:
                 print("Good choice!")
+        elif atm.isalpha():
+            print("Atm Pin need to be only numbers!")
         else:
             print("Good ATM Pin.")
             restart = input("Doy you want to go again? (yes/no) ")
